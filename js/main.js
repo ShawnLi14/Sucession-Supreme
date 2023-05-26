4f12f17d2065862876ad158ec527903c42fb4f4f
Shawn <shmorganl14@gmail.com>
2023-05-25 22:50:31 -0400

    initial commut

---
+++ js/main.js
@@ -0,0 +1,103 @@
+let plantedBoxes = new Set();
+var lastBoxID = 0;
+
+$(document).ready(function() {
+    $('.grid-item').on('click', function() {
+        var boxNumber = parseInt($(this).attr('id'));
+        if (!plantedBoxes.has(boxNumber)) {
+            lastBoxID = boxNumber;
+            showPopup($(this), boxNumber);
+        }
+    });
+    function showPopup($box, boxNumber) {
+        var $popupContainer = $('#popup-container');
+        var $popup = $('<div class="popup">' + addShopItems() + '</div>');
+
+        // Position the pop-up box
+        var boxOffset = $box.offset();
+        var popupLeft = boxOffset.left + $box.outerWidth() + 10;
+        var popupTop = boxOffset.top - $popup.outerHeight() - 10;
+        $popup.css({ left: popupLeft, top: popupTop });
+
+        // Append the pop-up box to the container
+        $popupContainer.html($popup);
+
+        // Show the pop-up box
+        $popupContainer.show();
+
+        $('.shop-item').on('click', function() {
+            var id = $(this).attr('id');
+            if(id == "Lichen"){
+                plant(lastBoxID, id.toLowerCase(), 10, 15, 100);
+            }
+            if(id == "Grass"){
+                plant(lastBoxID, id.toLowerCase(), 50, 5, 200);
+            }
+            if(id == "Beans"){
+                plant(lastBoxID, id.toLowerCase(), 100, 15, 1000);
+            }
+            if(id == "Shrub"){
+                plant(lastBoxID, id.toLowerCase(), 700, 10, 5000);   
+            }
+            if(id == "Hydrangea"){
+                plant(lastBoxID, id.toLowerCase(), 750, 20, 10000);   
+            }
+            if(id == "Pin Cherry"){
+                plant(lastBoxID, id.toLowerCase(), 6000, 7, 30000);
+            }
+            if(id == "Spruce"){
+                plant(lastBoxID, id.toLowerCase(), 5000, 15, 50000);
+            }
+            if(id == "Pine"){
+                plant(lastBoxID, id.toLowerCase(), 15000, 10, 100000);
+            }
+            if(id == "Fir"){
+                plant(lastBoxID, id.toLowerCase(), 20000, 20, 200000);
+                firTrees++;
+            }
+        });
+    }
+
+    // Close the pop-up box when clicking outside of it
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('.popup').length && !$(e.target).closest('.grid-item:not(.disabled-grid)').length) {
+            $('#popup-container').hide();
+        }
+    });
+});
+
+function addShopItems(){
+    var lichen = createShopItem("Lichen", 100, "images/plantable/lichen.png");
+    var str = lichen;
+    if(phase > 0){
+        var grass = createShopItem("Grass", 200, "images/plantable/grass.png");
+        var beans = createShopItem("Beans", 1000, "images/plantable/beans.png");
+        var shrub = createShopItem("Shrub", 5000, "images/plantable/shrub.png");
+        var hydrangea = createShopItem("Hydrangea", 10000, "images/plantable/hydrangea.png");
+        str += grass + beans + shrub + hydrangea;
+    }
+    if(phase > 1){
+        var pin_cherry = createShopItem("Pin Cherry", 30000, "images/plantable/pin cherry.png");
+        var spruce = createShopItem("Spruce", 50000, "images/plantable/spruce.png");
+        var pine = createShopItem("Pine", 100000, "images/plantable/pine.png");
+        var fir = createShopItem("Fir", 200000, "images/plantable/fir.png");
+        str += pin_cherry + spruce + pine + fir;
+    }
+    return str;
+}
+
+function createShopItem(name, cost, png){
+    return '<div class="shop-item" id="' + name + '"><img src="' + png + '" alt="Label" class="label-img"><div class="item-name">' + name + '</div><div class="item-cost">' + cost + ' Nutrients</div></div>';
+}
+
+function startGame(diff){
+    difficulty = diff;
+    $('#game').show();
+    $('#difficultySelect').hide();
+    $('body').addClass('body-game');
+}
+
+function difficultyScreen(){
+    $('#mainMenu').hide();
+    $('#difficultySelect').show();
+}
---
+++ js/plants.js
@@ -0,0 +1,201 @@
+var nutrients = 19999;
+var soilLevel = 90;
+var phase = 0;
+var firTrees = 0;
+var difficulty = 0;
+
+function updateSoil() {
+    if(soilLevel < 100){
+        $('.primary-succession-animation').css('animation', `colorChange 100s`);
+        $('.primary-succession-animation').css('animation-delay', `-${soilLevel}s`);
+        $('.primary-succession-animation').css('animation-play-state', 'paused');
+    }
+    else{
+        if(phase == 0){
+            secondPhase();
+        }
+        if(phase == 1 && nutrients >= 20000 && difficulty == 0){
+            thirdPhaseEasy();
+        }
+        $('.primary-succession-animation').css('animation-delay', `-95s`);
+    }
+}
+
+function winChecker(){
+    if(firTrees >= 20 && phase == 2){
+        winEasy();
+    }
+}
+
+setInterval(winChecker, 100);
+setInterval(updateSoil, 100);
+  
+function plant(id, img, perSecond, length, cost) {
+    if(nutrients < cost){
+        alert("Not enough nutrients!");
+        return;
+    }
+    else{
+        nutrients -= cost;
+        updateNutrients();
+    }
+    var seconds = 0;
+    plantImage(id, img);
+    $('#' + id).attr('data-content', length - seconds + 's left');
+    var interval = setInterval(function() {
+        if (seconds >= length) {
+            clearInterval(interval);
+            removePlant(id);
+        } else {
+            nutrients += perSecond;
+            soilLevel += 1;
+            updateNutrients();
+            seconds++;
+            $('#' + id).attr('data-content', length - seconds + 's left');
+        }
+    }, 1000);
+}
+
+function updateNutrients(){
+    $('#nutrientsValue').html(nutrients);
+    $('#soilLevel').html(Math.min(100, soilLevel) + '%');
+}
+function plantImage(id, img){
+    plantedBoxes.add(id);
+    $('#popup-container').hide();
+    $('#' + id).html('<img src="images/plantable/' + img + '.png" width="100" height="100">');
+}
+function removePlant(id){
+    plantedBoxes.delete(id);
+    $('#' + id).attr('data-content', "Plantable");
+    $('#' + id).html('');
+    $('#' + id).removeClass('animate');
+}
+$(document).ready(function() {
+    updateNutrients();
+});
+
+function secondPhase(){
+    phase = 1;
+    $('#goal').html('Reach 20000 nutrients');
+    $('body').addClass('phase-2-bg');
+    $('.level-animation-overlay').css('opacity', '1');
+    $('.next-level').css('animation-play-state', 'running');
+    $('.level-animation-overlay').css('z-index', '9999');
+    document.getElementById('rain-button').style.visibility = 'visible';
+    setTimeout(function() {
+        $('.level-animation-overlay').css('opacity', '0');
+        $('.level-animation-overlay').css('z-index', '-1');
+    }, 3000);
+}
+
+function activateRain(){
+    if(nutrients < 1000){
+        alert("Not enough nutrients!");
+        return;
+    }
+    $('#rain-button').css('visibility', 'hidden');
+    nutrients -= 1000;
+    updateNutrients();
+
+    var $overlay = $('#raindrop-overlay');
+    var $cloud = $('#cloud');
+    var $raindropsContainer = $('#raindrops-container');
+  
+    $overlay.css('display', 'block');
+    createRaindrops();
+    animateCloud();
+  
+    setTimeout(function() {
+        $('#5').removeClass('disabled-grid');
+        $('#9').removeClass('disabled-grid');
+    }, 1500);
+    setTimeout(function() {
+        $('#8').removeClass('disabled-grid');
+        $('#12').removeClass('disabled-grid');
+    }, 3000);
+    function animateCloud() {
+      $cloud.css('animation', 'cloud-move 5s linear forwards');
+    }
+  
+    $cloud.on('animationend', function() {
+      $overlay.css('display', 'none');
+      $cloud.css('animation', 'none');
+    });
+  
+    function createRaindrops() {
+      var numRaindrops = 30;
+  
+      for (var i = 0; i < numRaindrops; i++) {
+        var $raindrop = $('<div class="raindrop"></div>');
+        $raindrop.css('left', (Math.random() * $cloud.outerWidth() * 0.8 + $cloud.outerWidth() * 0.2) + 'px');
+        $raindrop.css('animation-delay', Math.random() * 3000 + 'ms');
+        $raindropsContainer.append($raindrop);
+      }
+    }
+}
+
+function thirdPhaseEasy(){
+    phase = 2;
+    $('#goal').html('Plant 20 Fir Trees');
+    $('.next-level').css('animation-play-state', 'running');
+    $('#next-level-text').html('Shrub Phase Complete!');
+    $('body').addClass('phase-3-bg');
+    $('.level-animation-overlay').css('opacity', '1');
+    $('.level-animation-overlay').css('z-index', '9999');
+    const animation = document.querySelector('.next-level');
+    animation.classList.add('reset-animation');
+    void animation.offsetWidth;
+    animation.classList.remove('reset-animation');
+    document.getElementById('river-button').style.visibility = 'visible';
+    setTimeout(function() {
+        $('.level-animation-overlay').css('opacity', '0');
+        $('.level-animation-overlay').css('z-index', '-1');
+    }, 3000);
+}
+
+function winEasy(){
+    phase = 3;
+    $('#next-level-text').html('Climax Community Created! You Win!');
+    $('.level-animation-overlay').css('opacity', '1');
+    $('.next-level').css('animation-play-state', 'running');
+    $('.level-animation-overlay').css('z-index', '9999');
+    $('.next-level').css('animation-iteration-count', '1');
+    const animation = document.querySelector('.next-level');
+    animation.classList.add('reset-animation');
+    void animation.offsetWidth;
+    animation.classList.remove('reset-animation');
+}
+
+function activateRiver(){
+    if(nutrients < 50000){
+        alert("Not enough nutrients!");
+        return;
+    }
+    $('#river-button').css('visibility', 'hidden');
+    nutrients -= 50000;
+    updateNutrients();
+
+    var riverAnimation = document.getElementById('river-animation-one');
+    riverAnimation.innerHTML = '<div class="river"></div>';
+    var riverAnimation = document.getElementById('river-animation-two');
+    riverAnimation.innerHTML = '<div class="river"></div>';
+
+    setTimeout(function() {
+        $('#1').removeClass('disabled-grid');
+        $('#13').removeClass('disabled-grid');
+    }, 1750);
+    setTimeout(function() {
+        $('#2').removeClass('disabled-grid');
+        $('#14').removeClass('disabled-grid');
+    }, 2500);
+    setTimeout(function() {
+        $('#3').removeClass('disabled-grid');
+        $('#15').removeClass('disabled-grid');
+    }, 3000);
+    setTimeout(function() {
+        $('#4').removeClass('disabled-grid');
+        $('#16').removeClass('disabled-grid');
+    }, 3500);
+}
+  
